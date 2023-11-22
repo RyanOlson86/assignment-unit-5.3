9f4e76d0d8aae9df25e0f78405696cf1ea01c4d9
Ryan Olson <ryan.olson86@yahoo.com>
2023-11-22 15:41:26 -0600

    Created seperate js and html files for extra stretch goals. Created addTracks function to create array of objects

--- assignment/scripts/extra-stretch-collection.js
+++ assignment/scripts/extra-stretch-collection.js
@@ -1,19 +1,41 @@
-// See indexExtra.html and extra-stretch-collection.js for Extra stretch goals
-
 console.log("***** Music Collection *****");
 // Safe Zone -- Write code below this line
 let myCollection = [];
+
+// function to create array of tracks with object properties name and duration (inputs are strings)
+function createTracks(trackArray, song, duration){
+    const newTracks = {
+        name: song,
+        duration: duration
+    };
+    trackArray.push(newTracks);
+    return newTracks;
+}
+
+let chiefTracks = [];
+createTracks(chiefTracks, "Creepin'", '3:52' );
+createTracks(chiefTracks, 'Drink in my hand', '3:11');
+createTracks(chiefTracks, 'Keep On', '2:38');
+console.log(chiefTracks);
+
+let americanaTracks = [];
+createTracks(americanaTracks, "The Kids Aren't Alright", '3:00');
+createTracks(americanaTracks, "Feelings", '2:52');
+createTracks(americanaTracks, "She's Got Issues", '3:48');
+console.log(americanaTracks);
+
 console.log('Starting collection:', myCollection);
 
 // NOTE --- ALL FUNCTIONS AT TOP,  CONSOLE.LOG TESTING AT BOTTOM
 
 // Function to collect an existing array (collection), and 3 strings (title, artist, yearPublished)
-function addToCollection(collection, title, artist, yearPublished) {
+function addToCollection(collection, title, artist, yearPublished, tracks) {
   // Create newAddition object with title, artist and yearPublished
   const newAddition = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
+    tracks: tracks
   };
   collection.push(newAddition); // push object to collection
   return newAddition; // return object added to collection
@@ -71,35 +93,13 @@ function search(collection, searchCriteria) {
 }
 
 
-// //Function for Artist AND Year search --- DOESN'T PASS
-// --- THIS CODE DOES NOT PASS PRIME TESTING, ALTHOUGH PASSES CONSOLE.LOG TESTING ---
-// Prime test passes yearPublished to search function NOT year
-//
-// function search(collection, searchCriteria) {
-//   //checks if searchCriteria is empty, undefined or missing info - return full collection
-//   if (
-//     typeof searchCriteria === "undefined" ||
-//     Object.keys(searchCriteria).length === 0 ||
-//     searchCriteria.artist == "" ||
-//     searchCriteria.year == ""
-//   ) {
-//     return collection; // return input collection if no search object, object is empty or missing artist/year
-//   } else {
-//     let artistSearch = findByArtist(collection, searchCriteria.artist);
-//     //artistSearch is an array of objects if artist is in collection, else it is empty
-//     if (artistSearch.length > 0) {
-//       return findByYear(artistSearch, searchCriteria.year);
-//     } else return artistSearch;
-//   }
-// }
-
 // Coding for testing addToCollection;
 console.log('--- Start adding 6 albums to collection ---');
-console.log(addToCollection(myCollection, "Chief", "Eric Church", "2011"));
+console.log(addToCollection(myCollection, "Chief", "Eric Church", "2011", chiefTracks));
 console.log(addToCollection(myCollection, "Carolina", "Eric Church", "2009"));
 console.log(addToCollection(myCollection, "Blacksheep", "Yelawolf", "2021"));
 console.log(addToCollection(myCollection, "Licensed to Ill", "Beastie Boys", "1986"));
-console.log(addToCollection(myCollection, "Americana", "The Offspring", "1998"));
+console.log(addToCollection(myCollection, "Americana", "The Offspring", "1998", americanaTracks));
 console.log(addToCollection(myCollection, "Sunset Man", "James Otto", "2008"));
 console.log('Collection after adding albums:', myCollection);
 
@@ -122,18 +122,3 @@ console.log("test Eric Church and 2011 (expect 1 result)", search(myCollection, 
 console.log("test for Eric Church(right artist) and 1900(wrong year) (expect empty)", search(myCollection, { artist: "Eric Church", year: "1900" }));
 console.log("test for Taylor Swift(wrong artist) and 2011(right year) (expect empty)", search(myCollection, { artist: "Eric Church", year: "1900" }));
 console.log("test no matching artist/year(expect empty)", search(myCollection, { artist: "Taylor Swift", year: "2000" }));
-
-// PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
-// as a lil' chunk of friendly code that you don't need to understand right now.
-// (It's used for automated testing.)
-try {
-  module.exports = {
-    myCollection: typeof myCollection !== "undefined" ? myCollection : undefined,
-    addToCollection: typeof addToCollection !== "undefined" ? addToCollection : undefined,
-    showCollection: typeof showCollection !== "undefined" ? showCollection : undefined,
-    findByArtist: typeof findByArtist !== "undefined" ? findByArtist : undefined,
-    search: typeof search !== "undefined" ? search : undefined,
-  };
-} catch (e) {
-  // Do nothing
-}
